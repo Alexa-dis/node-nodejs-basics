dea63f0ba5c71787a6829f17891ceb8f0af001b9
Aliaksandra Safonava <alexa-09@bk.ru>
2024-01-27 20:37:32 +0300

    implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,37 @@
+import fs from "fs/promises";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const fullPath = join(__dirname, "files");
+
+async function isDirectoryExists(path) {
+  try {
+    await fs.access(path, fs.constants.F_OK);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
 const list = async () => {
-    // Write your code here 
+  const files = [];
+  const isSrcExist = await isDirectoryExists(fullPath);
+  try {
+    if (isSrcExist) {
+      let inners = await fs.readdir(fullPath, { withFileTypes: true });
+      inners.forEach((inner) => {
+        inner.isDirectory()
+          ? getFiles(`${fullPath}/${inner.name}`)
+          : files.push(inner.name);
+      });
+      console.log(files);
+    } else {
+        throw new Error("FS operation failed");
+      }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await list();
