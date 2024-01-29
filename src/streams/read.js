ebf982304241428cfa4eb263ef4b285bb18cc0b8
Aliaksandra Safonava <alexa-09@bk.ru>
2024-01-29 17:30:51 +0300

    feat: read stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import fs from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const fileName = "fileToRead.txt";
+const fullPath = join(__dirname, "files", fileName);
+
 const read = async () => {
-    // Write your code here 
+  const fileStream = fs.createReadStream(fullPath);
+  fileStream.pipe(process.stdout);
 };
 
 await read();
