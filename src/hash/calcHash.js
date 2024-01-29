dc072b23ccdc30504c8ef780f7e29af93c6fd8ee
Aliaksandra Safonava <alexa-09@bk.ru>
2024-01-29 17:30:32 +0300

    feat: hash calcilation

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import fs from "fs";
+import crypto from "crypto";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const fileName = "fileToCalculateHashFor.txt";
+const fullPath = join(__dirname, "files", fileName);
+
+const hash = crypto.createHash("sha256");
+const fileStream = fs.createReadStream(fullPath);
+
 const calculateHash = async () => {
-    // Write your code here 
+  fileStream.on("data", (data) => {
+    hash.update(data);
+  });
+
+  fileStream.on("end", () => {
+    const fileHash = hash.digest("hex");
+    console.log(fileHash);
+  });
 };
 
 await calculateHash();
