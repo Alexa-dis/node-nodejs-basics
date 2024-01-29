a11274fa1de8798a3128a404d382563c137e0ad5
Aliaksandra Safonava <alexa-09@bk.ru>
2024-01-29 17:31:02 +0300

    feat: write stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import fs from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const fileName = "fileToWrite.txt";
+const fullPath = join(__dirname, "files", fileName);
+
 const write = async () => {
-    // Write your code here 
+  const fileStream = fs.createWriteStream(fullPath);
+  process.stdin.setEncoding("utf8");
+  process.stdin.pipe(fileStream);
 };
 
 await write();
