98138beac029b54a4d3ef27b671c8b6a1ccbf5f7
Aliaksandra Safonava <alexa-09@bk.ru>
2024-01-29 23:19:11 +0300

    feat: child processes

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,15 @@
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { fork } from "child_process";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const fullPath = join(__dirname, "files", "script.js");
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const forked = fork(fullPath, args, {silent: true});
+    process.stdin.pipe(forked.stdin)
+    forked.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( [ "someArgument1" , "someArgument2" , "someArgument3"]);
